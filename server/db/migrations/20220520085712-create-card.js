aecb5c17d692487efe0d1b9dc1c1ce49a0ee60d9
Igor Karasev <Igor932010@mail.ru>
2022-05-20 14:45:04 +0300

    server gotov

---
+++ server/db/migrations/20220520085712-create-card.js
@@ -0,0 +1,41 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Cards', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      question: {
+        type: Sequelize.TEXT,
+      },
+      answer: {
+        type: Sequelize.TEXT,
+      },
+      themeId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Themes',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+      },
+      cost: {
+        type: Sequelize.INTEGER,
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Cards');
+  },
+};
